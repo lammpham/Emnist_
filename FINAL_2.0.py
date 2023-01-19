6842cbc579df7321948d764e3ff1d958ed4433ba
lammpham <116561955+lammpham@users.noreply.github.com>
2023-01-19 15:02:10 +0100

    Update FINAL_2.0.py

--- FINAL_2.0.py
+++ FINAL_2.0.py
@@ -8,24 +8,24 @@ from picamera import PiCamera
 import cv2
 import numpy
 
+#Trainiertes Netzwerk wieder einlesen
 mlp = pickle.load(open("MLP_classifier_200",'rb'))
-i = 1
 
+#Instantiiere Kameralaufzeit
 camera = PiCamera()
 
-
+i = 1
 while i < 10:
     inp = input("Press Enter to predict Letter!\nOr press Q to quit.\nTo see the Camera view, press C.")
     if inp == 'Q':
         break
     if inp == 'C':
         camera.start_preview(fullscreen=False, window=(200,200,800,800))
-        
-    #print(inp)
+    # Aufnehmen einen Bild und Speichern asl "Letter"
     camera.capture(f'Letter.jpg')
     print("Captured Letter")
-
     file = r'/home/pi/Desktop/Final/Letter.jpg'
+    
     handwritten = []
     
     gray_image= cv2.imread(file, cv2.IMREAD_GRAYSCALE)
@@ -43,24 +43,15 @@ while i < 10:
 
     plt.imshow(cropped_image, cmap='gray')
     cv2.imwrite('Letter.jpg', img_resized)
-    #plt.show()
 
-    # INFERENZ auf RASPBERRY
     # Inferenz mit MLP Modell
-
-    #print('Versuchen wir es mit dem Bild')
     test_digit = io.imread('Letter.jpg')
-    #test_digit_prediction = mlp.predict(test_digit.reshape(1,784))
     single_item_array = (numpy.array(test_digit)).reshape(1,784)
     prediction = mlp.predict(single_item_array)
     typestory = typestory + str(chr(prediction[0]+64))
     typestory2 = typestory2 + str(chr(prediction[0]+96))
-    #labels_txt=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     
-    #print(f'test_digit_prediction: {test_digit_prediction[0]}')
-    #if prediction in range(prediction[0]+64):
     print("Predicted Capital Letter:",typestory)
-    #if prediction in range(prediction[0]+96):
     print("Predicted Lowercase Letter:",typestory2)
 
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7, 7))
